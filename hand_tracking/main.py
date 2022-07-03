89f7d4ed98313b8e19be2d0622d5d43b47857ca1
andreocunha <andreoliveiracunha20@gmail.com>
2022-07-03 11:53:39 -0300

    atualizações do modelo 3d

--- hand_tracking/main.py
+++ hand_tracking/main.py
@@ -33,7 +33,7 @@ conectedToServer = False
 @sio.event
 def connect():
     print("Conectado!")
-    sio.emit('data', "Hello World")
+    sio.emit('data', "Leitura da mão iniciada")
 
 @sio.event
 def connect_error():
@@ -60,46 +60,51 @@ while True:
     if results.multi_hand_landmarks:
 
         for i in results.multi_handedness:
-                label = MessageToDict(i)['classification'][0]['label']
-                if(label == 'Right'):
-                    for handLms in results.multi_hand_landmarks: 
+            label = MessageToDict(i)['classification'][0]['label']
+            
+            if(label == 'Right'):
+                for handLms in results.multi_hand_landmarks: 
 
-                        #handLMs sao 21 pontos da mão.
-                        lmList = []
-                        for id, lm in enumerate(handLms.landmark):
-                            # print(id, lm)
-                            #lm = x,y cordinate of each landmark in float numbers. lm.x, lm.y methods
-                            #So, need to covert in integer
-                            h, w, c =img.shape
-                            cx, cy = int(lm.x * w), int(lm.y * h)
-                            # print(id, cx, cy)
-                            lmList.append([id, cx, cy])
-                            # if id == 4: #(To draw 4th point)
-                            cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                    #handLMs sao 21 pontos da mão.
+                    lmList = []
+                    for id, lm in enumerate(handLms.landmark):
+                        # print(id, lm)
+                        #lm = x,y cordinate of each landmark in float numbers. lm.x, lm.y methods
+                        #So, need to covert in integer
+                        h, w, c =img.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        # print(id, cx, cy)
+                        lmList.append([id, cx, cy])
+                        # if id == 4: #(To draw 4th point)
+                        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                    # print(lmList)
+
+                    if len(lmList) != 0:
                         # print(lmList)
+                        fingers = []
 
-                        if len(lmList) != 0:
-                            fingers = []
-
-                            # Dedao
-                            if lmList[tipIds[0]][1] < lmList[tipIds[0]-1][1]:
+                        # Dedao
+                        if lmList[tipIds[0]][1] < lmList[5][1] and lmList[5][1] < lmList[9][1]:
+                            fingers.append(1)
+                        else:
+                            if lmList[tipIds[0]][1] > lmList[5][1] and lmList[5][1] > lmList[9][1]:
                                 fingers.append(1)
                             else:
                                 fingers.append(0)
 
-                            # Para os outros 4 dedos
-                            for id in range(1,5):
-                                if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
-                                    fingers.append(1)
-                                else:
-                                    fingers.append(0)
-                            print(fingers)
-                            if conectedToServer:
-                                sio.emit('data', fingers)
+                        # Para os outros 4 dedos
+                        for id in range(1,5):
+                            if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                                fingers.append(1)
+                            else:
+                                fingers.append(0)
+                        print(fingers)
+                        if conectedToServer:
+                            sio.emit('data', fingers)
 
-                        #drawing points and lines(=handconections)
-                        mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
-                
+                    #drawing points and lines(=handconections)
+                    mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(),mp_drawing_styles.get_default_hand_connections_style())
+            
     
     cv2.imshow('image', img)
     if cv2.waitKey(1)==27:
--- hand_tracking/teste.py
+++ hand_tracking/teste.py
@@ -1,82 +1,104 @@
-import cv2
 import mediapipe as mp
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+import uuid
+import os
+
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For static images:
-IMAGE_FILES = []
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
-    image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+joint_list = [[8,7,6], [12,11,10], [16,15,14], [20,19,18]]
 
-    # Print handedness and draw hand landmarks on the image.
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
-      mp_drawing.draw_landmarks(
-          annotated_image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS,
-          mp_drawing_styles.get_default_hand_landmarks_style(),
-          mp_drawing_styles.get_default_hand_connections_style())
-    cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-    # Draw hand world landmarks.
-    if not results.multi_hand_world_landmarks:
-      continue
-    for hand_world_landmarks in results.multi_hand_world_landmarks:
-      mp_drawing.plot_landmarks(
-        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
 
-# For webcam input:
+def get_label(index, hand, results):
+    output = None
+    for idx, classification in enumerate(results.multi_handedness):
+        if classification.classification[0].index == index:
+            
+            # Process results
+            label = classification.classification[0].label
+            score = classification.classification[0].score
+            text = '{} {}'.format(label, round(score, 2))
+            
+            # Extract Coordinates
+            coords = tuple(np.multiply(
+                np.array((hand.landmark[mp_hands.HandLandmark.WRIST].x, hand.landmark[mp_hands.HandLandmark.WRIST].y)),
+            [640,480]).astype(int))
+            
+            output = text, coords
+            
+    return output
+
+def draw_finger_angles(image, results, joint_list):
+    
+    # Loop through hands
+    for hand in results.multi_hand_landmarks:
+        #Loop through joint sets 
+        for joint in joint_list:
+            a = np.array([hand.landmark[joint[0]].x, hand.landmark[joint[0]].y]) # First coord
+            b = np.array([hand.landmark[joint[1]].x, hand.landmark[joint[1]].y]) # Second coord
+            c = np.array([hand.landmark[joint[2]].x, hand.landmark[joint[2]].y]) # Third coord
+            
+            radians = np.arctan2(c[1] - b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+            angle = np.abs(radians*180.0/np.pi)
+            
+            if angle > 180.0:
+                angle = 360-angle
+                
+            cv2.putText(image, str(round(angle, 2)), tuple(np.multiply(b, [640, 480]).astype(int)),
+                       cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    return image
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: 
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # BGR 2 RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Flip on horizontal
+        image = cv2.flip(image, 1)
+        
+        # Set flag
+        image.flags.writeable = False
+        
+        # Detections
+        results = hands.process(image)
+        
+        # Set flag to true
+        image.flags.writeable = True
+        
+        # RGB 2 BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Detections
+        print(results)
+        
+        # Rendering results
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                         )
+                
+                # Render left or right detection
+                if get_label(num, hand, results):
+                    text, coord = get_label(num, hand, results)
+                    cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            # Draw angles to image from joint list
+            draw_finger_angles(image, results, joint_list)
+            
+        # Save our image    
+        #cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
+        cv2.imshow('Hand Tracking', image)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
 cap.release()
+cv2.destroyAllWindows()
